2d859ed327c81731f4d71578712aacf35a6c439e
RoeeCarmeli <Roee.carmeli@post.runi.ac.il>
2025-01-09 12:46:05 +0200

    Update Network.java

--- Network.java
+++ Network.java
@@ -111,9 +111,10 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "Network:\n"; 
+        String ans = "Network:"; 
     for (int i = 0; i < userCount; i++) {
-        ans += users[i].toString() + "\n"; 
+        System.out.println();
+        ans += users[i].toString(); 
     }
     return ans;
     }
